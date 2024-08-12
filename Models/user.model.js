0589d7f1f63e4bc49ea62826cfb6d878dbac473a
Dimkha <dimkha516@gmail.com>
2024-08-12 21:08:24 +0000

    users

--- Models/user.model.js
+++ Models/user.model.js
@@ -2,24 +2,21 @@ const mongoose = require("mongoose");
 
 const userModel = mongoose.Schema(
   {
-    email: {
-      nomComplet: {
-        type: String,
-        required: true,
-      },
-      phone: {
-        type: Number,
-        required: true,
-        unique: true,
-      },
-      adress: {
-        type: String,
-      },
+    nomComplet: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    }, 
+    adress: {
+      type: String,
     },
   },
   {
     timestamps: true,
   }
-);
-
+); 
+ 
 module.exports.UsersModel = mongoose.model("users", userModel);
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const connectDB = require('./Connection/connect');
-const app = express();
-const dotenv = require('dotenv').config();
-const port = process.env.PORT;
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./Connection/connect");
+const usersRoutes = require("./Routes/user.routes");
 
+const app = express();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
 
 // CONNEXION TO CLUSTER:
 connectDB();
 
+// MIDDLEWARES:
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//
+app.use("/users", usersRoutes);
 
 app.listen(port, () => {
-    console.log("running on port: " + port);
-    
-})
+  console.log("running on port: " + port);
+});
