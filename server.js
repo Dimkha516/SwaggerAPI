d4ec2a0de8116ae6a983f29b9152134d52a8af20
Dimkha <dimkha516@gmail.com>
2024-08-13 16:23:48 +0000

    documentation

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./Connection/connect");
 const usersRoutes = require("./Routes/user.routes");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
 
+// CHARGER LE FICHIER YAML:
+const swaggerDocument = YAML.load("swagger.yaml");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // CONNEXION TO CLUSTER:
 connectDB();
 
@@ -15,6 +21,8 @@ connectDB();
 // app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 //
 app.use("/users", usersRoutes);
